0a69499eecbd6fd7ce2800ac9ead6ba32024d6fe
grahk93 <grace.juster@flatironschool.com>
2016-06-24 10:48:56 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{self.first_name} #{self.last_name}"
+  end
   
+  def list_roles
+    self.characters.collect do |character| #NAILED IT 
+      "#{character.name} - #{Show.find_by(character.show_id).name}"
+    end
+  end
+
 end
+
+   # TODO create a list_roles method
+    # TODO: build a method on actor that will return an array of
+    # strings in the form "#{character_name} - #{show_name}"
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+     "#{self.name} always says: #{self.catchphrase}"
+  end
   
 end
+# .catchphrase 
+ #TODO: make a method in the model to say his name and catchphrase
+    #remember the model is yours do with as you please, you 
+    #are free to add methods that perform actions on the model's data
